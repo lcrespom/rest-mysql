69e76cb13c7bc91603cf02dc8b6bbb45d01e9d3b
Luis Crespo <lcrespom@gmail.com>
2016-09-16 22:44:06 +0200

    Update recent addresses upon ride creation

--- example/rides.js
+++ example/rides.js
@@ -1,6 +1,7 @@
 var db = require('../src/db');
 var crudRouter = require('../src/crud-router');
 
+var RECENT_ADDR_LEN = 10;
 
 var routeConfig = {
 	"url": "/rides",
@@ -13,17 +14,36 @@ function registerRoute(router, dbconn) {
 	var url = routeConfig.url;
 	var rideTH = db.getCrudHandler(dbconn, routeConfig.table);
 	var addrTH = db.getCrudHandler(dbconn, 'addresses');
+	var custTH = db.getCrudHandler(dbconn, 'customers');
 	router.route(url).get((req, res) => {
 		res.json({ message: 'get pending' });
 	});
 	router.route(url).post((req, res) => {
-		postRide(req, res, rideTH, addrTH, url);
+		postRide(req, res, rideTH, addrTH, custTH, url);
 	});
 	router.route(url).put((req, res) => {
 		res.json({ message: 'put pending' });
 	});
 	router.route(url).delete((req, res) => {
 		res.json({ message: 'delete pending' });
+	});
+}
+
+function postRide(req, res, rideTH, addrTH, custTH, url) {
+	var ride = req.body;
+	checkNewAddress(ride.fromAddress, addrTH, (err1, result1) => {
+		checkNewAddress(ride.toAddress, addrTH, (err2, result2) => {
+			if (err1 || err2)
+				return crudRouter.handleError(err1 ? err1 : err2, res);
+			updateRecentAddresses(ride.customerId, result1, result2, custTH);
+			var backendRide = mapColumns(ride, rideColumns);
+			rideTH.create(backendRide, (err, result) => {
+				if (err)
+					return crudRouter.handleError(err, res);
+				var id = result.insertId;
+				res.status(201).json(crudRouter.addSelfLink({ id }, req, url, id));
+			});
+		});
 	});
 }
 
@@ -60,23 +80,6 @@ function mapColumns(src, map) {
 	return dst;
 }
 
-function postRide(req, res, rideTH, addrTH, url) {
-	var ride = req.body;
-	checkNewAddress(ride.fromAddress, addrTH, err1 => {
-		checkNewAddress(ride.toAddress, addrTH, err2 => {
-			if (err1 || err2)
-				return crudRouter.handleError(err1 ? err1 : err2, res);
-			var backendRide = mapColumns(ride, rideColumns);
-			rideTH.create(backendRide, (err, result) => {
-				if (err)
-					return crudRouter.handleError(err, res);
-				var id = result.insertId;
-				res.status(201).json(crudRouter.addSelfLink({ id }, req, url, id));
-			});
-		});
-	});
-}
-
 function checkNewAddress(addr, thandler, cb) {
 	// Empty address: set address_id to null
 	if (!addr.street && !addr.town && !addr.name) {
@@ -84,9 +87,36 @@ function checkNewAddress(addr, thandler, cb) {
 		return cb();
 	}
 	// Valid address_id: great, nothing to do
-	if (addr.id != -1) return cb();
+	if (addr.id != -1)
+		return cb(null, { insertId: addr.id });
 	// If address_id == -1, we need to create the address
 	thandler.create(addr, cb);
+}
+
+function updateRecentAddresses(customerId, r1, r2, thandler) {
+	if (isNaN(customerId)) return;
+	if (!r1 && !r2) return;
+	var a1 = -1, a2 = -1;
+	if (r1) a1 = r1.insertId;
+	if (r2) a2 = r2.insertId;
+	thandler.byId(customerId, (err, rows) => {
+		if (rows.length != 1) return;
+		var recent_addrs = rows[0].recent_addrs;
+		var recent;
+		if (recent_addrs == null || recent_addrs.length == 0)
+			recent = [];
+		else
+			recent = recent_addrs.split(',').map(x => +x);
+		if (a1 >= 0) recent = addRecent(recent, a1);
+		if (a2 >= 0) recent = addRecent(recent, a2);
+		recent.splice(RECENT_ADDR_LEN);
+		recent_addrs = recent.join(',');
+		thandler.update(customerId, { recent_addrs });
+	});
+}
+
+function addRecent(arr, e) {
+	return [e].concat(arr.filter(x => x != e));
 }
 
 //------------------------------ Exports ------------------------------
